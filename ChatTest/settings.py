c0128ebaf38cd56a39c2b439d236ca99dde27507
Filip <icespeaker07@gmail.com>
2022-05-10 21:20:13 +0200

    added redis

--- ChatTest/settings.py
+++ ChatTest/settings.py
@@ -136,7 +136,19 @@ MEDIA_URL = '/media/'
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [os.environ.get('REDIS_URL')],
+        },
+    },
+}
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": [os.environ.get('REDIS_URL')],
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        }
     }
 }
 
