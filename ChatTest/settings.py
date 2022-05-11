7e56e9eb85b68359aee91208f773834e17f12459
Filip <icespeaker07@gmail.com>
2022-05-11 19:13:14 +0200

    Replaced secret key with environ var

--- ChatTest/settings.py
+++ ChatTest/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wb4b_+bf8_au3z_0vj9wsx$2mp#8a3&*w)4e9o+*g5!wwtnml6'
+SECRET_KEY = os.environ["SECRET_KEY"]
 DEBUG = True
 
 ALLOWED_HOSTS = ['.railway.app','localhost', '127.0.0.1']
